4bcb6b31d3204ba3f030310131a6c3ede32415e3
wayneliang(梁宇浩) <wayneliang@futunn.com>
2021-10-12 21:19:28 +0800

    123

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,17 @@ const App = () => {
     navigate(page);
   };
 
+  const a = 1;
+
+  React.useLayoutEffect(() => {
+    console.log("123");
+  }, [a]);
+
   return (
     <div>
       <button onClick={() => changeRoute("/page1")}>/page1</button>
       <button onClick={() => changeRoute("/page2")}>/page2</button>
+      <h2>铁打的内容</h2>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./App.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 render(
-  <Router>
+  <BrowserRouter>
     <App />
-  </Router>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
